afeab787cb93855fc6eedb617617b2e96636a176
Leleco1604 <leonardogbarbosamelo2003@gmail.com>
2023-10-04 13:22:49 -0300

    primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Elementos do menu responsivo
+    var menuMobile = document.querySelector('.mobile-menu');
+    var menuIcon = document.querySelector('.mobile-menu-icon button');
+    var navLinks = document.querySelectorAll('.mobile-menu .nav-link');
+
+    // Elementos do formulário de login
+    var loginForm = document.getElementById("loginForm");
+    var usuarioInput = document.getElementById("usuario");
+    var senhaInput = document.getElementById("senha");
+
+    // Simular dados de login corretos
+    var usuarioCorreto = "usuario";
+    var senhaCorreta = "senha123";
+
+    // Função para abrir/fechar o menu responsivo
+    function toggleMenu() {
+        if (menuMobile.classList.contains('open')) {
+            menuMobile.classList.remove('open');
+            menuIcon.querySelector('img').src = "assets/img/menu_white_36dp.svg";
+        } else {
+            menuMobile.classList.add('open');
+            menuIcon.querySelector('img').src = "assets/img/close_white_36dp.svg";
+        }
+    }
+
+    // Evento de clique no ícone do menu
+    menuIcon.addEventListener('click', toggleMenu);
+
+    // Evento de clique nos links do menu
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+            toggleMenu(); // Fecha o menu ao clicar em um link
+        });
+    });
+
+    // Evento de clique em qualquer lugar fora do menu para fechá-lo
+    document.addEventListener('click', function(event) {
+        if (!menuMobile.contains(event.target) && !menuIcon.contains(event.target)) {
+            menuMobile.classList.remove('open');
+            menuIcon.querySelector('img').src = "assets/img/menu_white_36dp.svg";
+        }
+    });
+
+    // Evento de envio do formulário de login
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        var usuario = usuarioInput.value;
+        var senha = senhaInput.value;
+
+        if (usuario === usuarioCorreto && senha === senhaCorreta) {
+            // Redireciona o usuário para a página de sucesso após o login
+            window.location.href = "sucesso-login.html";
+        } else {
+            alert("Usuário ou senha incorretos. Tente novamente.");
+        }
+    });
+});
